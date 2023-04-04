1283a744039685bca1ee6005f227035557d62785
mythpoy <okmlnsunok@gmail.com>
2023-04-04 22:48:40 +0900

    feat : UsernameAuthenticationToken 객체를 구성 후 SecurityContextHolder에 적용

--- src/main/java/com/example/demo/security/filter/TokenCheckFilter.java
+++ src/main/java/com/example/demo/security/filter/TokenCheckFilter.java
@@ -1,5 +1,6 @@
 package com.example.demo.security.filter;
 
+import com.example.demo.security.APIUserDetailsService;
 import com.example.demo.security.exception.AccessTokenException;
 import com.example.demo.util.JWTUtil;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -7,6 +8,9 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -21,6 +25,7 @@ import java.util.Map;
 public class TokenCheckFilter extends OncePerRequestFilter {
     // OncePerRequestFilter는 하나의 요청에 대해서 한번씩 동작하는 필터
     // JWT 토큰을 검사하는 역할을 하는 필터 (로그인 했는지)
+    private final APIUserDetailsService apiUserDetailsService;
     private final JWTUtil jwtUtil;
 
     @Override
@@ -30,18 +35,37 @@ public class TokenCheckFilter extends OncePerRequestFilter {
 
         String path = request.getRequestURI();
 
-        if(!path.startsWith("/api/")) {
+
+        if (!path.startsWith("/api/")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        log.info("Token check Filter....");
+        log.info("Token Check Filter..........................");
         log.info("JWTUtil: " + jwtUtil);
 
-        try {
-            validateAccessToken(request);
-            filterChain.doFilter(request, response);
-        } catch(AccessTokenException accessTokenException) {
+
+
+        try{
+
+            Map<String, Object> payload = validateAccessToken(request);
+
+            //mid
+            String mid = (String)payload.get("mid");
+
+            log.info("mid: " + mid);
+
+            UserDetails userDetails = apiUserDetailsService.loadUserByUsername(mid);
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            filterChain.doFilter(request,response);
+        }catch(AccessTokenException accessTokenException){
             accessTokenException.sendResponseError(response);
         }
     }
@@ -77,4 +101,5 @@ public class TokenCheckFilter extends OncePerRequestFilter {
             throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.EXPIRED);
         }
     }
+
 }
